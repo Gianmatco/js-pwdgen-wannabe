18ac1a950a87764dfeaa002be5b9395a9534e2eb
Gianmatco <gianmarco.culqui.ikaros@gmail.com>
2022-03-23 21:59:23 +0100

    uploading

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Chiedi all’utente il suo nome,
+//poi chiedi il suo cognome,
+//poi chiedi il suo colore preferito
+//Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+
+let userName = prompt('Come ti chiami ?');
+
+let lastName = prompt('quale il tuo cognome ?');
+
+let color = prompt('quale il tuo colore preferito ?')
+
+
+
+
+
+
+document.getElementById('password').innerHTML += userName + lastName + color;
+
